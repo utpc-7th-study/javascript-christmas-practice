dd3fd68e692237a307421f758a79b887676cc1f7
BK <3031011@naver.com>
2023-12-08 00:18:35 +0900

    chore: 상수 객체 키 변경

--- src/constant/eventDate.js
+++ src/constant/eventDate.js
@@ -1,6 +1,14 @@
-const EVENT_DATE = Object.freeze({
-  start: 1,
-  end: 31,
+import deepFreeze from '../utils/deepFreeze.js';
+
+const EVENT_DATE = deepFreeze({
+  december: {
+    start: 1,
+    end: 31,
+  },
+  xMas: {
+    start: 1,
+    end: 25,
+  },
 });
 
 export default EVENT_DATE;
--- src/constant/krBenefit.js
+++ src/constant/krBenefit.js
@@ -1,6 +1,6 @@
 const KR_BENEFIT_DATA = Object.freeze({
-  weekDay: '평일 할인',
-  weekEnd: '주말 할인',
+  weekday: '평일 할인',
+  weekend: '주말 할인',
   special: '특별 할인',
   dDay: '크리스마스 디데이 할인',
 });
