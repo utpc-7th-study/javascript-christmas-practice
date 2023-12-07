e8adf8446b0ec930e0f860a3a9ce4d52daf2112a
rlaclghks123 <rlaclghks123@naver.com>
2023-12-07 17:34:30 +0900

    fix: 객체 변수 키워드 문제 수정

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,13 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
-    // ...
-}
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+  printMenu() {
+    Console.print('<주문 메뉴>');
+  },
+
+  print(message) {
+    Console.print(message);
+  },
+};
+
+export default OutputView;
