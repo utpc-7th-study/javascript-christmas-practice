cfaf05e42ff4bc01cfe19a0b301ccd3728551307
rlaclghks123 <rlaclghks123@naver.com>
2023-12-07 17:45:55 +0900

    feat: 방문날짜 유효성 검사 - 숫자 형태가 아닌 경우

--- src/domain/DateManager.js
+++ src/domain/DateManager.js
@@ -2,7 +2,17 @@ class DateManager {
   #visitDate;
 
   constructor(visitDate) {
+    this.#validate(visitDate);
+
     this.#visitDate = visitDate;
+  }
+
+  #validate(visitDate) {
+    const ERROR_MESSAGE = '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.';
+
+    if (!/^[0-9]+$/.test(visitDate)) {
+      throw new Error(ERROR_MESSAGE);
+    }
   }
 }
 
